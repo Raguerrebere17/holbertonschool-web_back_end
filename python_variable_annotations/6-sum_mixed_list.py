2d5570300a0a78136552503b4125ec5fe505e166
Raguerrebere17 <raguerrebere17@gmail.com>
2023-09-03 13:27:53 -0700

    Task number 6

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Task 6 - sum mixed list
+"""
+from typing import Union, List
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """
+    Function sum_mixed_list
+    """
+    x = 0.0
+    for i in mxd_lst:
+        x = i + x
+    return x

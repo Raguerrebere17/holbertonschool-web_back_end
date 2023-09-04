b879ead16c9fb836fda2d5c89970bf57abb396b9
Raguerrebere17 <raguerrebere17@gmail.com>
2023-09-03 19:33:54 -0700

    Task number 0

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Task number 0
+"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """Asynchronous coroutine"""
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay

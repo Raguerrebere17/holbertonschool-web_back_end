652c6ab83bf0bcbed14cdb9b8e403876909fe70c
Raguerrebere17 <raguerrebere17@gmail.com>
2023-09-03 19:44:08 -0700

    Syntax error fixed (space between 'end' and '=')

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -13,6 +13,6 @@ def measure_time(n: int, max_delay: int) -> float:
     wait_n: __import__('1-concurrent_coroutines').wait_n
     start = time.time()
     asyncio.run(wait_n(n, max_delay))
-    end= time.time()
+    end = time.time()
     total_time = end - start
     return (total_time / n)
